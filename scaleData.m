857fd8a9b9cf7b7c341f4788a71b8754cdecf61f
ianddwyer <128656623+ianddwyer@users.noreply.github.com>
2024-09-26 05:16:08 -0600

    scaleData Update_0.3

--- scaleData.m
+++ scaleData.m
@@ -1,12 +1,6 @@
 
-%% CHECK SIG FIGS FOR GENERAL AUTOSCALING FUNCTION
-%test with below in console
-%[scaledData,scaling,label] = scaleData(0.1); 
-%fprintf("input value: %f\nrescale with: %f\nunit prefix: %s\n",scaledData,scaling,label)
-
-%% THE FUNCTION
-
 %float scalar and vectors should be currently stable, cautious of matrices
+%[scaledData,scaling,label] = scaleData(0.1); fprintf("input value: %f\nrescale with: %f\nunit prefix: %s\n",scaledData,scaling,label)
 function [scaledData,scaling,label] = scaleData(num)
         try
             %print stored location to avoid forgetting to include this for other users
@@ -18,37 +12,18 @@ function [scaledData,scaling,label] = scaleData(num)
                 number = num;
             end
         
-            %init sigfig counters
-            sigfigs_left =  -1;
-            sigfigs_right = 0;
-            
-            %counts sig figs to left of decimal
-            numleft = floor(number);
-            numright = number-floor(number);
-        
             %find label from mag, init vars
             label = "";
             mag = 0;
             
-            while (abs(numleft)>0)
-                numleft = floor(numleft/10);
-                sigfigs_left = sigfigs_left+1;
-            end
-            
-            %counts sig figs to right based on number of zeros before useful value
-            numleft = floor(number); %redeclare for logical comparisons
-            while (double(abs(numright))<1 && abs(numleft)<=0)
-                numright = numright*10;
-                sigfigs_right = sigfigs_right+1;
-            end
-    
+            %counts sig figs to right and left of zero
+            sigfigs_left = ceil(log10(floor(number)))-1;
+            sigfigs_right = floor(log10(1/(number-floor(number))));
             
             %prefer to scale towards larger magnitude using conditional
-            if sigfigs_left>0
-                %reduce size for large values
+            if sigfigs_left>0 %reduce size for large values
                 scaling = 10^-(sigfigs_left-1);
-            else
-                %increase size for small values
+            else %increase size for small values
                 scaling = 10^(sigfigs_right);
             end
             
@@ -59,57 +34,55 @@ function [scaledData,scaling,label] = scaleData(num)
                 if mod(sigfigs_right,3)==0
                     mag = mag-1;
                 end
-                if mag == 0
-                    label = "";
-                elseif mag == 1
-                    label = "m"; %milli
-                elseif mag == 2
-                    label = "u"; %micro
-                elseif mag == 3
-                    label = "n"; %nano
-                elseif mag == 4
-                    label = "p"; %pico
-                elseif mag == 5
-                    label = "f"; %femto
-                elseif mag>5
-                    error("ERROR: Number too small for correct output")
+                switch mag
+                    case 0
+                        label = "";
+                    case 1
+                        label = "m"; %milli
+                    case 2
+                        label = "u"; %micro
+                    case 3
+                        label = "n"; %nano
+                    case 4
+                        label = "p"; %pico
+                    case 5
+                        label = "f"; %femto
+                    otherwise
+                        error("ERROR: Number too small for correct output")
                 end
-            
             %check for increase scaling 
             elseif scaling<1
                 %test number of 3s places
                 mag = floor((sigfigs_left)/3);
                 if mag <= 0
                     label = "";
-                elseif mag == 1
-                    label = "k"; %kilo
-                elseif mag == 2
-                    label = "M"; %mega
-                elseif mag == 3
-                    label = "G"; %giga
-                elseif mag == 4
-                    label = "T"; %Tera
-                elseif mag == 5
-                    label = "P"; %peta
-                elseif mag>5
-                    error("ERROR: Number too large for correct output")
+                else
+                    switch mag
+                        case 1
+                            label = "k"; %kilo
+                        case 2
+                            label = "M"; %mega
+                        case 3
+                            label = "G"; %giga
+                        case 4
+                            label = "T"; %Tera
+                        case 5
+                            label = "P"; %peta
+                        otherwise
+                            error("ERROR: Number too large for correct output")
+                    end
                 end
             end
-            
-            
+
             %prefer to scale towards larger magnitude using conditional
             %redefine scaling after obtaining label
             if sigfigs_left>0
                 scaling = 10^-(mag*3);
-                %figs = sigfigs_left-1;
             else  
                 scaling = 10^(mag*3);
-                %figs = sigfigs_right;
             end
             scaledData = scaling*num;
         catch
             error(sprintf("\nUsage:\n\t[scalednum,scaling,label] = scaleData(number)"))
-        end
-
-    
+        end   
 end
